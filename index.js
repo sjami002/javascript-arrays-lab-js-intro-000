d0b45c2676b47d04b3ec137907cb4f75134a188f
Shoptorshi Jamil <sjami002@gmail.com>
2019-01-24 15:29:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 var kittens = ["Milo", "Otis", "Garfield"]; //define your array here
-
+var kitty = "Rocco"
 // Add your functions and code here
-// function destructivelyAppendKitten()
+function destructivelyAppendKitten() {
+  kittens.unshift(kitty);
+  return kittens;
+}
